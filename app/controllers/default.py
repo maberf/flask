525ac5b164b4615651d7ad6e044b5b289f11c4d7
Maber Fernandes <71026050+maberf@users.noreply.github.com>
2020-10-19 21:26:51 -0300

    Update
    Update

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,7 +1,13 @@
-from flask import render_template
-from app import app, db
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user
+from app import app, db, login_manager
 from app.models.tables import User
 from app.models.forms import LoginForm
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.filter_by(id=id).first()
 
 
 @app.route("/index")
@@ -14,16 +20,29 @@ def index():
 def login():
     form = LoginForm()  # instância na página de login
     if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
+        user = User.query.filter_by(username=form.username.data).first()  
+        if user and user.password == form.password.data:
+        # se usuário existe e a senha digitada confere com o valor no BD
+            login_user(user)
+            return redirect(url_for("index"))  # após login vai para index
+            flash("Logged in.")
+        else:
+            flash("Invalid login.")
     return render_template('login.html', form=form)
 
 
-@app.route("/test/<info>")  # página de teste de inserção no BD
-@app.route("/test/", defaults={'info': None})
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("Logged out.")
+    return redirect(url_for("index"))  # após logout vai para index
+
+"""
+@app.route("/test/<info>")  # teste do BD
+@app.route("/test/", defaults={"info": None})
 def test(info):
-    i = User("uiliam", "1234", "Uiliam Zeferino", "uiliamzeferino@hotmail.com")
-    db.session.add(i)  # abre sessão no BD para inserção de instância i
-    db.session.commit(i)  # grava (persiste) no BD a inserção da instância i
+    r = User.query.filter_by(username="uiliam").first()  # lê BD
+    db.session.delete(r)  # deleta registro do BD
+    db.session.commit()  # comita (persiste) no BD
     return "OK"
-    
+"""
